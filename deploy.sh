9e29bedfdb0fff0301d503bfb1f29673c92b19e5
Mat Wood <thepacketgeek@users.noreply.github.com>
2021-12-30 12:22:03 -0800

    Adding notes for fedora cross-compilation

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Prereqs:
+# MacOS Prereqs:
 # https://dev.to/h_ajsf/cross-compiling-rust-for-raspberry-pi-4iai
 # brew install arm-linux-gnueabihf-binutils
 # rustup target add armv7-unknown-linux-musleabihf
@@ -15,9 +15,10 @@
 set -o errexit
 
 readonly TARGET_HOST=r2d2
+readonly TARGET=armv7-unknown-linux-musleabihf
 readonly TARGET_PATH=/home/pi/bin/rpi
-readonly SOURCE_PATH=./target/armv7-unknown-linux-musleabihf/release/rpi
+readonly SOURCE_PATH=./target/$TARGET/release/rpi-simon-says
 
-cargo build --release -v
+cargo build --release
 scp ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
 ssh -t ${TARGET_HOST} ${TARGET_PATH}
