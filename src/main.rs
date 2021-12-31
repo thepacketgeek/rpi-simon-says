0832eb679bafecaad9033827ec73aeb6b3d5d6dc
Mat Wood <thepacketgeek@users.noreply.github.com>
2021-12-30 16:38:37 -0800

    Adding debounce to button example

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
 
-use rust_gpiozero::{Button, LED};
+use rust_gpiozero::{Button, LED, Debounce};
 
 fn main() {
-    let mut red_button = Button::new(4);
+    let mut red_button = Button::new(4).debounce(Duration::from_millis(500));
     let red_led = {
         let mut led = LED::new(17);
         led.set_blink_count(1);
         Arc::new(Mutex::new(led))
     };
-    let mut green_button = Button::new(5);
+    let mut green_button = Button::new(5).debounce(Duration::from_millis(500));
     let green_led = {
         let mut led = LED::new(23);
         led.set_blink_count(1);
@@ -34,17 +34,14 @@ fn main() {
         let led = red_led.clone();
         red_button
             .when_pressed(move |_level| {
-                println!("pressed red");
                 led.lock().unwrap().blink(0.3, 0.1);
-            })
-            .unwrap();
+            }).unwrap();
         let led = green_led.clone();
         green_button
             .when_pressed(move |_level| {
                 println!("pressed green");
                 led.lock().unwrap().blink(0.3, 0.1);
-            })
-            .unwrap();
+            }).unwrap();
     }
 
     std::thread::sleep(Duration::from_secs(3600));
